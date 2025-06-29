20c27f4a28ae5f7855b9cbc24a049295c7a70da2
Adam Dehnel <arsdehnel@gmail.com>
2025-06-29 08:14:48 -0500

    fix: use the right type for ctx object

--- src/app/layouts/standard.tsx
+++ src/app/layouts/standard.tsx
@@ -1,28 +1,28 @@
-import { RequestInfo } from "rwsdk/worker";
+import { AppContext } from "@/worker";
 
 import '../styles/global.css';
 
 export default function StandardLayout({ children, ctx }:
-	{ children: React.ReactNode, ctx: Object }) {
+	{ children: React.ReactNode, ctx: AppContext }) {
 	return (
 		<>
-		<header>
-			<div className="menu">
-				<a href="/">Home</a>
-				<a href="/trails">Trails</a>
-				<a href="/log">Hiking Log</a>
-				<a href="/maps">Offline Maps</a>
-			</div>
-			<div className="user-info">
-				{ctx.user?.username
-					? `You are logged in as user ${ctx.user.username}`
-					: "You are not logged in"}
-			</div>
-		</header>
-		<main>
-			{children}
-		</main>
-		<footer>Footer</footer>
+			<header>
+				<div className="menu">
+					<a href="/">Home</a>
+					<a href="/trails">Trails</a>
+					<a href="/log">Hiking Log</a>
+					<a href="/maps">Offline Maps</a>
+				</div>
+				<div className="user-info">
+					{ctx.user?.username
+						? `You are logged in as user ${ctx.user.username}`
+						: "You are not logged in"}
+				</div>
+			</header>
+			<main>
+				{children}
+			</main>
+			<footer>Footer</footer>
 		</>
 	);
 }
