cfbb93f61d81c20c2a734f36ce4ef1a7b471049a
Adam Dehnel <arsdehnel@gmail.com>
2025-06-29 06:38:29 -0500

    fix: correct the trails model migration

--- migrations/0002_add_initial_trail_model.sql
+++ migrations/0002_add_initial_trail_model.sql
@@ -1,1 +1,15 @@
--- This is an empty migration.
+-- CreateTable
+CREATE TABLE "Trail" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "imageURL" TEXT NOT NULL,
+    "pdfURL" TEXT NOT NULL,
+    "videoURL" TEXT NOT NULL,
+    "dnrURL" TEXT NOT NULL,
+    "difficulty" INTEGER NOT NULL,
+    "notes" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME,
+    "deletedAt" DATETIME
+);
