1469dd7c41caa590c90122d7a60b9f848960c918
Adam Dehnel <arsdehnel@gmail.com>
2025-07-05 09:41:19 -0500

    fix: adjust video sizing to full width

--- src/app/components/trail-video.tsx
+++ src/app/components/trail-video.tsx
@@ -4,12 +4,8 @@ import YouTube from 'react-youtube';
 export function TrailVideo( { videoURL }: { videoURL: string } ) {
 
     const videoOpts = {
-      height: '390',
-      width: '640',
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 1,
-      },
+      // height: '390',
+      width: '100%'
     };
 
     let videoId;
