49949dab94f97bb756ad472ba4e0beb667772321
Adam Dehnel <arsdehnel@gmail.com>
2025-07-06 08:49:59 -0500

    feat: initial signup page

---
+++ src/app/pages/user/Signup.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useState, useTransition } from "react";
+import {
+    startRegistration,
+} from "@simplewebauthn/browser";
+import {
+    finishPasskeyRegistration,
+    startPasskeyRegistration,
+} from "./functions";
+import { link } from '@/app/shared/links';
+
+export const Signup = () => {
+
+    const [username, setUsername] = useState("");
+    const [result, setResult] = useState("");
+    const [isPending, startTransition] = useTransition();
+
+    const passkeyRegister = async () => {
+        // 1. Get a challenge from the worker
+        const options = await startPasskeyRegistration(username);
+
+        // 2. Ask the browser to sign the challenge
+        const registration = await startRegistration({ optionsJSON: options });
+
+        // 3. Give the signed challenge to the worker to finish the registration process
+        const success = await finishPasskeyRegistration(username, registration);
+
+        if (!success) {
+            setResult("Registration failed");
+        } else {
+            window.location.href = link("/user/login");
+        }
+    };
+
+    const handlePerformPasskeyRegister = () => {
+        startTransition(() => void passkeyRegister());
+    };
+
+    return (
+        <>
+            <div>Signup</div>
+            <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+            />
+            <button onClick={handlePerformPasskeyRegister} disabled={isPending}>
+                {isPending ? <>...</> : "Register with passkey"}
+            </button>
+            {result && <div>{result}</div>}
+        </>
+    )
+}
